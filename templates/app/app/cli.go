c8ad62bc8a0a5f0161858191446a759a7fe7c7d4
Anton Novojilov <andy@essentialkaos.com>
2021-07-16 01:46:49 +0300

    Add new template for application with spec and config

--- templates/app/app/cli.go
+++ templates/app/app/cli.go
@@ -1,4 +1,4 @@
-package daemon
+package app
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 //                                                                                    //
@@ -8,16 +8,20 @@ package daemon
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 import (
+	"fmt"
 	"os"
 	"strings"
 
 	"pkg.re/essentialkaos/ek.v12/fmtc"
+	"pkg.re/essentialkaos/ek.v12/fsutil"
 	"pkg.re/essentialkaos/ek.v12/knf"
 	"pkg.re/essentialkaos/ek.v12/log"
 	"pkg.re/essentialkaos/ek.v12/options"
-	"pkg.re/essentialkaos/ek.v12/pid"
-	"pkg.re/essentialkaos/ek.v12/signal"
 	"pkg.re/essentialkaos/ek.v12/usage"
+	"pkg.re/essentialkaos/ek.v12/usage/completion/bash"
+	"pkg.re/essentialkaos/ek.v12/usage/completion/fish"
+	"pkg.re/essentialkaos/ek.v12/usage/completion/zsh"
+	"pkg.re/essentialkaos/ek.v12/usage/man"
 	"pkg.re/essentialkaos/ek.v12/usage/update"
 
 	knfv "pkg.re/essentialkaos/ek.v12/knf/validators"
@@ -26,19 +30,24 @@ import (
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
-// Basic service info
+// Basic application info
 const (
 	APP  = "{{NAME}}"
 	VER  = "{{VERSION}}"
 	DESC = "{{DESC}}"
 )
 
+// ////////////////////////////////////////////////////////////////////////////////// //
+
 // Options
 const (
 	OPT_CONFIG   = "c:config"
 	OPT_NO_COLOR = "nc:no-color"
 	OPT_HELP     = "h:help"
-	OPT_VERSION  = "v:version"
+	OPT_VER      = "v:version"
+
+	OPT_COMPLETION   = "completion"
+	OPT_GENERATE_MAN = "generate-man"
 )
 
 // Configuration file properties
@@ -49,12 +58,6 @@ const (
 	LOG_LEVEL = "log:level"
 )
 
-// Pid file info
-const (
-	PID_DIR  = "/var/run/{{SHORT_NAME}}"
-	PID_FILE = "{{SHORT_NAME}}.pid"
-)
-
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 // optMap contains information about all supported options
@@ -62,13 +65,20 @@ var optMap = options.Map{
 	OPT_CONFIG:   {Value: "/etc/{{SHORT_NAME}}.knf"},
 	OPT_NO_COLOR: {Type: options.BOOL},
 	OPT_HELP:     {Type: options.BOOL, Alias: "u:usage"},
-	OPT_VERSION:  {Type: options.BOOL, Alias: "ver"},
+	OPT_VER:      {Type: options.BOOL, Alias: "ver"},
+
+	OPT_COMPLETION:   {},
+	OPT_GENERATE_MAN: {Type: options.BOOL},
 }
+
+// useRawOutput is raw output flag (for cli command)
+var useRawOutput = false
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 
+// Init is main function
 func Init() {
-	_, errs := options.Parse(optMap)
+	args, errs := options.Parse(optMap)
 
 	if len(errs) != 0 {
 		for _, err := range errs {
@@ -78,26 +88,56 @@ func Init() {
 		os.Exit(1)
 	}
 
+	preConfigureUI()
+
+	if options.Has(OPT_COMPLETION) {
+		os.Exit(genCompletion())
+	}
+
+	if options.Has(OPT_GENERATE_MAN) {
+		os.Exit(genMan())
+	}
+
 	configureUI()
 
-	if options.GetB(OPT_VERSION) {
+	if options.GetB(OPT_VER) {
 		os.Exit(showAbout())
 	}
 
-	if options.GetB(OPT_HELP) {
+	if options.GetB(OPT_HELP) || len(args) == 0 {
 		os.Exit(showUsage())
 	}
 
 	loadConfig()
 	validateConfig()
-	registerSignalHandlers()
 	setupLogger()
-	createPidFile()
 
-	log.Aux(strings.Repeat("-", 80))
-	log.Aux("%s %s starting…", APP, VER)
+	process(args)
+}
 
-	start()
+// preConfigureUI preconfigures UI based on information about user terminal
+func preConfigureUI() {
+	term := os.Getenv("TERM")
+
+	fmtc.DisableColors = true
+
+	if term != "" {
+		switch {
+		case strings.Contains(term, "xterm"),
+			strings.Contains(term, "color"),
+			term == "screen":
+			fmtc.DisableColors = false
+		}
+	}
+
+	if !fsutil.IsCharacterDevice("/dev/stdout") && os.Getenv("FAKETTY") == "" {
+		fmtc.DisableColors = true
+		useRawOutput = true
+	}
+
+	if os.Getenv("NO_COLOR") != "" {
+		fmtc.DisableColors = true
+	}
 }
 
 // configureUI configures user interface
@@ -136,15 +176,6 @@ func validateConfig() {
 	}
 }
 
-// registerSignalHandlers registers signal handlers
-func registerSignalHandlers() {
-	signal.Handlers{
-		signal.TERM: termSignalHandler,
-		signal.INT:  intSignalHandler,
-		signal.HUP:  hupSignalHandler,
-	}.TrackAsync()
-}
-
 // setupLogger confugures logger subsystems
 func setupLogger() {
 	err := log.Set(knf.GetS(LOG_FILE), knf.GetM(LOG_PERMS, 644))
@@ -160,38 +191,9 @@ func setupLogger() {
 	}
 }
 
-// createPidFile creates PID file
-func createPidFile() {
-	pid.Dir = PID_DIR
-
-	err := pid.Create(PID_FILE)
-
-	if err != nil {
-		printErrorAndExit(err.Error())
-	}
-}
-
-func start() {
+// process starts processing
+func process(args []string) {
 	// DO YOUR STUFF HERE
-}
-
-// intSignalHandler is INT signal handler
-func intSignalHandler() {
-	log.Aux("Received INT signal, shutdown…")
-	shutdown(0)
-}
-
-// termSignalHandler is TERM signal handler
-func termSignalHandler() {
-	log.Aux("Received TERM signal, shutdown…")
-	shutdown(0)
-}
-
-// hupSignalHandler is HUP signal handler
-func hupSignalHandler() {
-	log.Info("Received HUP signal, log will be reopened…")
-	log.Reopen()
-	log.Info("Log reopened by HUP signal")
 }
 
 // printError prints error message to console
@@ -210,31 +212,65 @@ func printErrorAndExit(f string, a ...interface{}) {
 	os.Exit(1)
 }
 
-// shutdown stops deamon
-func shutdown(code int) {
-	pid.Remove(PID_FILE)
-	os.Exit(code)
-}
-
 // ////////////////////////////////////////////////////////////////////////////////// //
 
 // showUsage prints usage info
 func showUsage() int {
-	info := usage.NewInfo()
-
-	info.AddOption(OPT_CONFIG, "Path to configuration file", "config")
-	info.AddOption(OPT_NO_COLOR, "Disable colors in output")
-	info.AddOption(OPT_HELP, "Show this help message")
-	info.AddOption(OPT_VERSION, "Show version")
-
-	info.Render()
-
+	genUsage().Render()
 	return 0
 }
 
 // showAbout prints info about version
 func showAbout() int {
-	usage := &usage.About{
+	genAbout().Render()
+	return 0
+}
+
+// genCompletion generates completion for different shells
+func genCompletion() int {
+	info := genUsage()
+
+	switch options.GetS(OPT_COMPLETION) {
+	case "bash":
+		fmt.Printf(bash.Generate(info, "{{SHORT_NAME}}"))
+	case "fish":
+		fmt.Printf(fish.Generate(info, "{{SHORT_NAME}}"))
+	case "zsh":
+		fmt.Printf(zsh.Generate(info, optMap, "{{SHORT_NAME}}"))
+	default:
+		return 1
+	}
+
+	return 0
+}
+
+// genMan generates man page
+func genMan() int {
+	fmt.Println(
+		man.Generate(
+			genUsage(),
+			genAbout(),
+		),
+	)
+
+	return 0
+}
+
+// genUsage generates usage info
+func genUsage() *usage.Info {
+	info := usage.NewInfo()
+
+	info.AddOption(OPT_CONFIG, "Path to configuration file", "config")
+	info.AddOption(OPT_NO_COLOR, "Disable colors in output")
+	info.AddOption(OPT_HELP, "Show this help message")
+	info.AddOption(OPT_VER, "Show version")
+
+	return info
+}
+
+// genAbout generates info about version
+func genAbout() *usage.About {
+	return &usage.About{
 		App:           APP,
 		Version:       VER,
 		Desc:          DESC,
@@ -243,8 +279,6 @@ func showAbout() int {
 		License:       "Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>",
 		UpdateChecker: usage.UpdateChecker{"essentialkaos/{{SHORT_NAME}}", update.GitHubChecker},
 	}
-
-	usage.Render()
-
-	return 0
 }
+
+// ////////////////////////////////////////////////////////////////////////////////// //
